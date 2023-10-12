fa7ae83fe7ea6072bdce5bfee7192bdc82a9d981
Sven Laht <Sven.Laht@ttptlive.ee>
2023-10-12 11:19:39 +0300

    b

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+import { ref } from 'vue';
+const message = ref('Neeger');
 </script>
 
 <template>
-
+  <button v-on:click="message='ok'">tapke mind lapidaga</button>
+  <input type="text" v-bind:value="message" @input="message=$event.target.value">
+  <h1>{{ message }}</h1>
 </template>
