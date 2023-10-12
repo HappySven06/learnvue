235c56f70e1abf84b63a6047997e43cb77b008ca
Sven Laht <Sven.Laht@ttptlive.ee>
2023-10-12 11:24:39 +0300

    c

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,7 @@ const message = ref('Neeger');
 <template>
   <button v-on:click="message='ok'">tapke mind lapidaga</button>
   <input type="text" v-bind:value="message" @input="message=$event.target.value">
-  <h1>{{ message }}</h1>
+  <input type="text" :value="message" @input="message=$event.target.value">
+  <input type="text" v-model="message">
+  <h1>{{ message.split('').reverse().join('') }}</h1>
 </template>
